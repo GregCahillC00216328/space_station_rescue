1904d4957fe247ce499978404caca3834f89ca7d
GregCahillC00216328 <gregcahill17@gmail.com>
2020-01-15 11:33:24 +0000

    Initial map

---
+++ ASSETS/Map/block.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="block" tilewidth="50" tileheight="50" tilecount="1" columns="1">
+ <image source="../IMAGES/THEMAP/block.png" width="50" height="50"/>
+</tileset>
---
+++ Map.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+class Map
+{
+public:
+	Map();
+	~Map();
+
+private:
+	sf::Vector2f mapArr[40][20];
+
+};
+
+
+
